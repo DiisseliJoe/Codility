820e285e4461fc719eaa3abba51a868126f3e0d5
DiisseliJoe <47569620+DiisseliJoe@users.noreply.github.com>
2019-10-28 15:07:01 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -26,7 +26,8 @@ namespace ConsoleApp4
             Console.WriteLine(result);
 
         }
-
+        
+        //solution is the answer to codility binarygap task
         public int solution(int N)
         {
             // write your code in C# 6.0 with .NET 4.5 (Mono)
