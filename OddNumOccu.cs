cd8dc5da7c3f3d513bb4edb14e99c25bd8bcec61
DiisseliJoe <47569620+DiisseliJoe@users.noreply.github.com>
2019-10-30 12:49:14 +0200

    Add files via upload

---
+++ OddNumOccu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddNumberOccurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Program p = new Program();
+
+            int[] Arr = new int[] {1, 2, 1, 3, 2, 4, 3, 5, 6, 4, 5, 7, 8, 7, 8};
+            a = p.solution(Arr);
+            Console.WriteLine(a);
+
+            Arr = new int[] { 1,2,3,4,1,2,4};
+            a = p.solution(Arr);
+            Console.WriteLine(a);
+
+            Arr = new int[] { 1,2,3,4,1,2,3,4,1,3,4,1,2,3,4};
+            a = p.solution(Arr);
+            Console.WriteLine(a);
+
+        }
+
+        public int solution(int[] A)
+        {
+            int result = 0;
+            foreach (int i in A)
+            {
+                result ^= i;
+            }
+            return result;
+        }
+    }
+
+}
+
